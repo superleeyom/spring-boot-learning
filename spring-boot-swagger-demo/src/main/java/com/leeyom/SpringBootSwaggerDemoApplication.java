1694eefc48c375621f2bfde054967d27b0f47e1b
leeyom <635709492@qq.com>
2017-09-20 15:48:26 +0800

    Spring Boot中使用Swagger2构建RESTful API 文档

---
+++ spring-boot-swagger-demo/src/main/java/com/leeyom/SpringBootSwaggerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.leeyom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwaggerDemoApplication.class, args);
+	}
+}
