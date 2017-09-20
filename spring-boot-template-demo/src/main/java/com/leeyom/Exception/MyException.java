2052dc1b7e977ec26a77292985110f2b757f69a0
leeyom <635709492@qq.com>
2017-09-20 22:55:07 +0800

    spring boot 配置 thymeleaf 模板引擎 & Web应用的统一异常处理

---
+++ spring-boot-template-demo/src/main/java/com/leeyom/Exception/MyException.java
@@ -0,0 +1,8 @@
+package com.leeyom.Exception;
+
+public class MyException extends Exception{
+
+    public MyException(String message) {
+        super(message);
+    }
+}
--- spring-boot-template-demo/src/main/java/com/leeyom/web/HelloController.java
+++ spring-boot-template-demo/src/main/java/com/leeyom/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.leeyom.web;
 
+import com.leeyom.Exception.MyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,4 +13,23 @@ public class HelloController {
         return "index";
     }
 
+    /**
+     * 测试统一异常处理类
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping("/hello")
+    public String hello() throws Exception {
+        throw new Exception("发生错误");
+    }
+
+    /**
+     * 测试异常处理类，返回 json 格式数据
+     * @return
+     * @throws MyException
+     */
+    @RequestMapping("/json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
+    }
 }
