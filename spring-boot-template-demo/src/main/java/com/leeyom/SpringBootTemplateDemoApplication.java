9a2c4513578034539881564306d5cb33fdb1a08d
leeyom <635709492@qq.com>
2017-09-20 11:47:46 +0800

    spring boot 配置 thymeleaf 模板引擎

---
+++ spring-boot-template-demo/src/main/java/com/leeyom/SpringBootTemplateDemoApplication.java
@@ -0,0 +1,12 @@
+package com.leeyom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTemplateDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTemplateDemoApplication.class, args);
+	}
+}
