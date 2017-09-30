bafa15d31d734741d0cf108a24915f249360754d
leeyom <635709492@qq.com>
2017-09-30 16:20:02 +0800

    Spring Boot与shiro整合

---
+++ spring-boot-shiro-demo/src/main/java/com/leeyom/springbootshirodemo/SpringBootShiroDemoApplication.java
@@ -0,0 +1,12 @@
+package com.leeyom.springbootshirodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShiroDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShiroDemoApplication.class, args);
+	}
+}
